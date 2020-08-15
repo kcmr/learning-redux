a05a2a91c4fda82a234f9f1774871221ce1915c4
Kus Cámara <kcmr@users.noreply.github.com>
2020-08-16 00:08:48 +0200

    use createSlice from redux toolkit

--- src/store/todos.js
+++ src/store/todos.js
@@ -1,26 +1,27 @@
-import { createAction, createReducer } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-export const addTodo = createAction('ADD_TODO');
-export const toggleTodo = createAction('TOGGLE_TODO');
-export const removeTodo = createAction('REMOVE_TODO');
+const slice = createSlice({
+  name: 'todos',
+  initialState: [],
+  reducers: {
+    addTodo: (todos, action) => {
+      todos.push({
+        id: Date.now(),
+        completed: false,
+        ...action.payload,
+      });
+    },
 
-// Reducer
-export default createReducer([], {
-  [addTodo.type]: (todos, action) => {
-    // we can write mutation code here (internally redux tookkit prevents mutation)
-    todos.push({
-      id: Date.now(),
-      completed: false,
-      ...action.payload,
-    });
-  },
+    toggleTodo: (todos, action) => {
+      const index = todos.findIndex((todo) => todo.id === action.payload.id);
+      todos[index].completed = !todos[index].completed;
+    },
 
-  [toggleTodo.type]: (todos, action) => {
-    const index = todos.findIndex((todo) => todo.id === action.payload.id);
-    todos[index].completed = !todos[index].completed;
-  },
-
-  [removeTodo.type]: (todos, action) => {
-    return todos.filter((todo) => todo.id !== action.payload.id);
+    removeTodo: (todos, action) => {
+      return todos.filter((todo) => todo.id !== action.payload.id);
+    },
   },
 });
+
+export const { addTodo, toggleTodo, removeTodo } = slice.actions;
+export default slice.reducer;
