8df8f8c0b43724d13fbb2472a66b675c5d2e14ac
Kus Cámara <kcmr@users.noreply.github.com>
2020-08-16 01:07:52 +0200

    refactor todos

--- src/store/todos.js
+++ src/store/todos.js
@@ -1,25 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const addTodo = (todos, action) => {
+  todos.push({
+    id: Date.now(),
+    completed: false,
+    ...action.payload,
+  });
+};
+
+const toggleTodo = (todos, action) => {
+  const index = todos.findIndex((todo) => todo.id === action.payload.id);
+  todos[index].completed = !todos[index].completed;
+};
+
+const removeTodo = (todos, action) => {
+  return todos.filter((todo) => todo.id !== action.payload.id);
+};
+
 const slice = createSlice({
   name: 'todos',
   initialState: [],
   reducers: {
-    addTodo: (todos, action) => {
-      todos.push({
-        id: Date.now(),
-        completed: false,
-        ...action.payload,
-      });
-    },
-
-    toggleTodo: (todos, action) => {
-      const index = todos.findIndex((todo) => todo.id === action.payload.id);
-      todos[index].completed = !todos[index].completed;
-    },
-
-    removeTodo: (todos, action) => {
-      return todos.filter((todo) => todo.id !== action.payload.id);
-    },
+    addTodo,
+    toggleTodo,
+    removeTodo,
   },
 });
 
