378d7430637a11adbfe4025ec35981030b17ecfd
Kus Cámara <kcmr@users.noreply.github.com>
2020-08-16 01:39:31 +0200

    add list of todos

--- src/app.js
+++ src/app.js
@@ -1,18 +1,57 @@
 import { LitElement, html } from 'lit-element';
 import { configStore } from './store/configStore.js';
+import { addTodo, removeTodo, toggleTodo } from './store/todos.js';
 import './components/TodosForm.js';
-import { addTodo } from './store/todos.js';
+import './components/TodoItem.js';
 
 const store = configStore();
 
 class ReduxApp extends LitElement {
+  static get properties() {
+    return {
+      _todos: {
+        type: Array,
+      },
+    };
+  }
+
+  constructor() {
+    super();
+
+    this._todos = [];
+    store.subscribe(() => (this._todos = store.getState()));
+  }
+
   _handleTodoAdded({ detail: value }) {
     store.dispatch(addTodo({ value }));
+  }
+
+  _handleTodoRemoved(id) {
+    store.dispatch(removeTodo({ id }));
+  }
+
+  _handleCompletedChanged(id) {
+    store.dispatch(toggleTodo({ id }));
   }
 
   render() {
     return html`
       <todos-form @todo-added=${this._handleTodoAdded}></todos-form>
+
+      <ul>
+        ${this._todos.map(
+          ({ value, completed, id }) => html`
+            <li>
+              <todo-item
+                value=${value}
+                ?completed=${completed}
+                @removed=${() => this._handleTodoRemoved(id)}
+                @completed-changed=${() => this._handleCompletedChanged(id)}
+              ></todo-item>
+            </li>
+          `,
+        )}
+      </ul>
     `;
   }
 }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,44 @@
+import { LitElement, html } from 'lit-element';
+
+class TodoItem extends LitElement {
+  static get properties() {
+    return {
+      value: {
+        type: String,
+      },
+      completed: {
+        type: Boolean,
+      },
+    };
+  }
+
+  _handleCheckboxChange() {
+    this.completed = !this.completed;
+
+    this.dispatchEvent(
+      new Event('completed-changed', {
+        detail: this.completed,
+      }),
+    );
+  }
+
+  _handleButtonClick() {
+    this.dispatchEvent(new Event('removed'));
+  }
+
+  render() {
+    return html`
+      <label>
+        <input
+          type="checkbox"
+          @change=${this._handleCheckboxChange}
+          .checked=${this.completed}
+        />
+        <span>${this.value}</span>
+        <button @click=${this._handleButtonClick}>Remove</button>
+      </label>
+    `;
+  }
+}
+
+customElements.define('todo-item', TodoItem);
