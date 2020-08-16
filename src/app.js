3425405adb746db5b251d46891d67e3d1e0e7c81
Kus Cámara <kcmr@users.noreply.github.com>
2020-08-17 00:08:53 +0200

    remove filter

--- src/app.js
+++ src/app.js
@@ -1,14 +1,8 @@
 import { LitElement, html, css } from 'lit-element';
 import store from './store/index.js';
-import {
-  addTodo,
-  removeTodo,
-  toggleTodo,
-  selectUndoneTasks,
-} from './store/todos.js';
+import { addTodo, removeTodo, toggleTodo } from './store/todos.js';
 import './components/TodosForm.js';
 import './components/TodoList.js';
-import './components/FilterBy.js';
 
 const getState = (slice) => {
   const state = store.getState();
@@ -46,17 +40,9 @@ class ReduxApp extends LitElement {
     store.dispatch(toggleTodo({ id: detail.id }));
   }
 
-  _handleFilterChanged({ detail: checked }) {
-    this._todos = checked ? selectUndoneTasks(getState()) : getState('todos');
-  }
-
   render() {
     return html`
       <todos-form @todo-added=${this._handleTodoAdded}></todos-form>
-      <filter-by
-        value="Undone"
-        @changed=${this._handleFilterChanged}
-      ></filter-by>
       <todo-list
         .items=${this._todos}
         @item-removed=${this._handleTodoRemoved}
